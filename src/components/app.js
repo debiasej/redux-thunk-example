48e9b30e1323c3ddbc4df4c76b814edc4f9c19ff
Mario de Biase <debiasej@gmail.com>
2017-01-08 21:27:55 +0100

    Create markup for the list of emails

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  componentWillMount() {
+  }
+
+  renderUser({id, name, email}) {
+    return (
+      <li clasName="list-group-item" key={id}>
+        <span clasName="label label default label-pill float-right">
+          <a href={email}>{email}</a>
+        </span>
+        {name}
+      </li>
+    );
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h4>Email directory</h4>
+        <ul clasName="list-group">
+        debiasej@gmail.com Mario de Biase
+        </ul>
+      </div>
     );
   }
 }
